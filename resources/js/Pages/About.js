bc511b9d45c74343c6cfa9f08ce1ce2822cb54bd
4H1R <=>
2021-08-30 10:15:36 +0430

    init

---
+++ resources/js/Pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function About() {
+    return (
+        <div>
+            <h1>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
+                cum magni corrupti fugiat reprehenderit distinctio earum,
+                consequuntur ut placeat? Dignissimos sint obcaecati et odit
+                possimus omnis nesciunt cumque, officiis repellendus?
+            </h1>
+        </div>
+    );
+}
+
+export default About;
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+require __DIR__ . '/auth.php';
+
+Route::get('/about', function () {
+    return inertia('About');
+});
